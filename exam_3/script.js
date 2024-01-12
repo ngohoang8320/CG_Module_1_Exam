6b8d3c1198c63cb4277af8050eabc61885cdd400
Ngo Hoang <ngohoang8320@gmail.com>
2024-01-12 11:20:23 +0700

    Add 4 folder for exam.

--- exam_3/script.js
+++ exam_3/script.js
@@ -1,10 +1,19 @@
-let number = 12
+let number = 15
 let sum = 0
 document.write('Number: ' + number)
 
 let sumOfListPrime = (number) => {
-    for (let i = 0; i <= number; i++) {
-        sum += i
+    for (let i = 2; i <= number; i++) {
+        let isPrime = true
+        for (let j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                isPrime = false
+                break
+            }
+        }
+        if (isPrime) {
+            sum += i
+        }
     }
     return sum
 }
